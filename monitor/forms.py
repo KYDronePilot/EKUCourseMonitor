2d3be2b41d7bd20ab7f88b707b05441a374730a0
KYDronePilot <33381603+KYDronePilot@users.noreply.github.com>
2018-12-31 16:30:18 -0500

    Created an email deactivation form.  - Overrided clean method to include checking for whether the activation    code was valid.

--- monitor/forms.py
+++ monitor/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Course
+from .models import Course, Email
 
 
 # For saving a new course to be monitored.
@@ -20,4 +20,40 @@ class NewMonitoredCourse(forms.ModelForm):
         widgets = {
             'crn': forms.TextInput(attrs={'placeholder': '12345'}),
             'name': forms.TextInput(attrs={'placeholder': 'ENG 101'})
+        }
+
+
+class DeactivateEmail(forms.Form):
+    """
+    Obtains deactivation code from the user.
+
+    Attributes:
+        code: The deactivation code.
+
+    """
+    code = forms.CharField(
+        max_length=20,
+        label='Deactivation Email'
+    )
+
+    def clean(self):
+        """
+        Validate the deactivation code.
+         - Add an error if there are no active emails associated with the entered code.
+
+        """
+        super(DeactivateEmail, self).clean()
+        code = self.cleaned_data.get('code')
+        emails = Email.objects.filter(
+            deactivation_code=code,
+            deactivated=False
+        )
+        # Add an error if no active emails associated with this code.
+        if not emails:
+            self.add_error('code', 'This code is either not valid or has already been used.')
+
+    class Meta:
+        # Placeholder for deactivation code.
+        widgets = {
+            'code': forms.TextInput(attrs={'placeholder': 'abcdefghij1234567890'})
         }
