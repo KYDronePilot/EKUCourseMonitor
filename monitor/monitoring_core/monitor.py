e3cfb14dea64a921b5ccc403aac212c30afb663a
KYDronePilot <>
2018-12-31 10:46:06 -0500

    Changes:  - Added initialization method to run on startup of monitoring script.  - Updated the way scanning is performed.  - A few other minor code improvements.

--- monitor/monitoring_core/monitor.py
+++ monitor/monitoring_core/monitor.py
@@ -23,8 +23,18 @@ class Monitor:
 
     def __init__(self):
         self.workers = []
-        # Start threads for each course that needs to be monitored.
-        # TODO: Finish here.
+        # Start threads for courses that should have them.
+        self.initialize()
+
+    def initialize(self):
+        """
+        Actions to perform on startup of the monitoring script.
+         - Start threads for all courses that should have them.
+
+        """
+        active_course_threads = Course.objects.filter(thread_active=True)
+        for course in active_course_threads:
+            self.new_worker(course)
 
     def new_worker(self, course):
         """
@@ -44,9 +54,10 @@ class Monitor:
         worker.start()
         self.workers.append(worker)
 
-    def close_workers(self, workers):
+    @staticmethod
+    def close_workers(workers):
         """
-        Close monitor threads for a list of courses.
+        Close monitor threads for a list of workers.
          - Set stop control for each worker.
          - Join worker threads.
 
@@ -59,55 +70,64 @@ class Monitor:
         for worker in workers:
             worker.join()
 
-    def get_new_courses(self):
+    def setup_new_courses(self):
         """
-        Get new courses from the DB that need to be monitored.
-         - Gets new courses and sets their thread status to active.
+        Check and setup monitoring on any new courses.
 
-        Returns: New courses to add to workers.
+        Returns:
+            True if new courses setup, False if not.
 
         """
         new_courses = Course.objects.filter(
             thread_active=False,
             is_monitored=True
         )
+        if not new_courses:
+            return False
+        # Set them to have active threads.
         for course in new_courses:
             course.thread_active = True
             course.save()
-        return new_courses
+        # Create workers for each one.
+        for course in new_courses:
+            self.new_worker(course)
+        return True
 
-    def get_deactivated_courses(self):
+    def close_deactivated_courses(self):
         """
-        Get courses that have been disabled and should not be monitored (deactivated).
-         - Gets deactivated courses and sets their thread status to not active.
+        Stop monitoring any deactivated courses.
 
-        Returns: Deactivated courses.
+        Returns: True if any were found, False if not.
 
         """
         deactivated_courses = Course.objects.filter(
             thread_active=True,
             is_monitored=False
         )
+        if not deactivated_courses:
+            return False
+        deactivated_courses = list(deactivated_courses)
+        # Set course thread status to deactivated.
         for course in deactivated_courses:
             course.thread_active = False
             course.save()
-        return deactivated_courses
+        # Close workers and remove from worker list.
+        self.close_workers(deactivated_courses)
+        for worker in deactivated_courses:
+            self.workers.remove(worker)
+        return True
 
     def scan(self):
         """
         Scans the DB, creating new threads for any new courses and closing
-        threads for any deactivated ones.
+        threads for any deactivated ones. Waits the timeout amount before
+        scanning again.
 
         """
-        # Get courses that should be monitored but aren't currently.
-        new_courses = list(self.get_new_courses())
-        # Start monitoring each new course.
-        for course in new_courses:
-            self.new_worker(course)
-        # Get deactivated courses that are being monitored.
-        deactivated_courses = list(self.get_deactivated_courses())
-        # Close threads for deactivated courses.
-        self.close_workers(deactivated_courses)
+        while True:
+            self.setup_new_courses()
+            self.close_deactivated_courses()
+            time.sleep(Monitor.TIMEOUT)
 
     def catch(self, signum, frame):
         """
