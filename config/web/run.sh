71eb5815f576c1dfa95563f2769508db4d297ae9
KYDronePilot <33381603+KYDronePilot@users.noreply.github.com>
2019-01-07 21:25:48 -0500

    Moved migrations to run script to prevent error on startup.

--- config/web/run.sh
+++ config/web/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Apply migrations.
+python /src/manage.py makemigrations
+python /src/manage.py migrate
 # Start monitor daemon.
 /usr/local/bin/python /src/monitor/monitoring_core/monitor.py &
 # Start Gunicorn
--- config/web/setup.sh
+++ config/web/setup.sh
@@ -6,6 +6,4 @@ pip install -r /config/requirements.pip
 # Set up Gunicorn script.
 chmod u+x /run/gunicorn_start
 # Set up Django.
-python manage.py makemigrations
-python manage.py migrate
 python manage.py collectstatic
